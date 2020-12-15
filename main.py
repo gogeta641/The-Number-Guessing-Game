f07269d111a7c9a435cd5a705130cdfebd6bdeb2
Karthik Ramachandran <karthik.gogeta@gmail.com>
2020-12-15 13:29:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import art
+import random
+
+print(art.logo)
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+chosen = random.randint(1, 100)
+
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+
+if difficulty == "easy":
+    counter = 10
+elif difficulty == "hard":
+    counter = 5
+
+guess = 0
+
+while guess != chosen:
+    while counter > 0:
+        print(f"You have {counter} attempts remaining to guess a number.")
+        guess = int(input("Make a guess: "))
+        if guess > chosen:
+            print("Too high.")
+        elif guess < chosen:
+            print("Too low.")
+        else:
+            print("Correct guess. You win.")
+            break
+        counter -= 1
+        if counter == 0:
+            print("You are out of chances. You lose")
+            break
+        print("Guess again.")
+        
